807cf21791857df9200eddeae62b49d528b45ed7
elafontaine <eric.lafontaine@bell.ca>
2021-02-15 08:51:02 -0500

    Refactor tests for parsing header

--- pdu_test.go
+++ pdu_test.go
@@ -13,36 +13,43 @@ var enquiryLinkFixture, _ = hex.DecodeString("00000010000000150000000000000000")
 var enquiryLinkRespFixture, _ = hex.DecodeString("00000010800000150000000000000000")
 var bindTransmitterFixture, _ = hex.DecodeString("0000001f000000020000000000000000746573740074657374000034000000")
 
-func TestEnquiryLinkParsing(t *testing.T) {
-	enquiryLinkObjHeader := PDU{
-		header: Header{16, "enquire_link", "ESME_ROK", 0},
-	}
+var enquiryLinkObjHeader = PDU{
+	header: Header{commandLength: 16, commandId: "enquire_link", commandStatus: "ESME_ROK", sequenceNumber: 0},
+}
 
-	if got, _ := parseHeader(enquiryLinkFixture); got != enquiryLinkObjHeader {
-		t.Errorf("didn't get enquire_link object %v, to be %v", got, enquiryLinkObjHeader)
+var enquiryLinkRespObjHeader = PDU{
+	header: Header{commandLength: 16, commandId: "enquire_link_resp", commandStatus: "ESME_ROK", sequenceNumber: 0},
+}
+
+var bindTransmitterObjHeader = PDU{
+	header: Header{commandLength: 31, commandId: "bind_transmitter", commandStatus: "ESME_ROK", sequenceNumber: 0},
+}
+
+func Test_parseHeaders(t *testing.T) {
+	type args struct {
+		bytes []byte
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantPdu PDU
+	}{
+		{"parse_enquire_link", args{bytes: enquiryLinkFixture}, enquiryLinkObjHeader},
+		{"parse_enquire_link", args{bytes: enquiryLinkRespFixture}, enquiryLinkRespObjHeader},
+		{"parse_bind_transmitter", args{bytes: bindTransmitterFixture}, bindTransmitterObjHeader},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, _ := parseHeader(tt.args.bytes)
+			if got != tt.wantPdu {
+				t.Errorf("parseHeader() got = %v, wantPdu %v", got, tt.wantPdu)
+				return
+			}
+		})
 	}
 }
 
-func TestParsePduEnquiryLinkResp(t *testing.T) {
-	enquiryLinkRespObjHeader := PDU{
-		header: Header{16, "enquire_link_resp", "ESME_ROK", 0},
-	}
-
-	if got, _ := parseHeader(enquiryLinkRespFixture); got != enquiryLinkRespObjHeader {
-		t.Errorf("didn't get enquire_link_resp object %v, to be %v", got, enquiryLinkRespObjHeader)
-	}
-}
-
-func TestBindTransmitterParsing(t *testing.T) {
-	bindTransmitterObjHeader := PDU{
-		header: Header{commandLength: 31, commandId: "bind_transmitter", commandStatus: "ESME_ROK", sequenceNumber: 0},
-	}
-	if got, _ := parseHeader(bindTransmitterFixture); got != bindTransmitterObjHeader {
-		t.Errorf("didn't get bind_transmitter object %v, to be %v", got, bindTransmitterFixture)
-	}
-}
-
-func Test_parseHeader(t *testing.T) {
+func Test_parseHeaderInvalidPdus(t *testing.T) {
 	type args struct {
 		bytes []byte
 	}
