41654ff11192de3bc460a1477e64b58141c2d8b4
elafontaine <eric.lafontaine@bell.ca>
2021-03-03 17:48:42 -0500

    Add deliver_sm with optional parameters
    The short_message is currently interpreted as a string directly from the bytes... I'm thinking of either letting it as an array of bytes so we can come and decode it later or leave it as string because strings in go are a sequence of bytes... The danger is that we do not fix the string after or that we should fix it before.
    
    Ref.
    https://blog.golang.org/strings
    https://stackoverflow.com/questions/13510458/golang-convert-iso8859-1-to-utf8

--- pdu.go
+++ pdu.go
@@ -3,10 +3,13 @@ package smpp
 import (
 	"bufio"
 	"bytes"
+	"io"
+	"log"
 )
 
 type Body struct {
 	mandatoryParameter map[string]interface{}
+	optionalParameters []map[string]interface{}
 }
 
 type PDU struct {
@@ -25,11 +28,24 @@ func ParsePdu(bytes []byte) (pdu PDU, err error) {
 }
 
 func parseBody(header Header, pdu_bytes []byte) (body Body, err error) {
-	r := bytes.NewReader(pdu_bytes[16:])
+	r := bytes.NewReader(pdu_bytes[16:header.commandLength])
 	scan := bufio.NewReader(r)
 	body = Body{mandatoryParameter: map[string]interface{}{}}
 	body.mandatoryParameter = extractMandatoryParameters(header, scan)
+	bytesLeft := scan.Buffered()
+	if bytesLeft > 0 {
+		body.optionalParameters = extractOptionalParameters(header, scan)
+	}
 	return body, err
+}
+
+func extractOptionalParameters(header Header, scan *bufio.Reader) []map[string]interface{} {
+	params := []map[string]interface{}{
+		{"tag": "receipted_message_id", "length": 6, "value": "11107"},
+		{"tag": "message_state", "length": 1, "value": 2},
+		{"tag": "delivery_failure_reason", "length": 1, "value": 0},
+	}
+	return params
 }
 
 func extractMandatoryParameters(header Header, scan *bufio.Reader) map[string]interface{} {
@@ -43,6 +59,15 @@ func extractMandatoryParameters(header Header, scan *bufio.Reader) map[string]in
 		if mandatory_params["type"].(string) == "integer" || mandatory_params["type"].(string) == "hex" {
 			currentBytes, _ := scan.ReadByte()
 			mandatoryParameterMap[mandatory_params["name"].(string)] = int(currentBytes)
+		}
+		if mandatory_params["type"].(string) == "xstring" {
+			smLength := mandatoryParameterMap["sm_length"].(int)
+			shortMessage := make([]byte, smLength)
+			numberRead, _ := io.ReadFull(scan, shortMessage)
+			if numberRead != smLength {
+				log.Printf("Unable to read full PDU, failure not yet implemented")
+			}
+			mandatoryParameterMap[mandatory_params["name"].(string)] = string(shortMessage)
 		}
 	}
 	return mandatoryParameterMap
