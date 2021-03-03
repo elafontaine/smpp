525e4b983d8f06d1fd3622c226bcb4e74ff997cc
elafontaine <eric.lafontaine1@gmail.com>
2021-03-03 17:48:42 -0500

    Add deliver_sm with optional parameters
    The short_message is currently interpreted as a string directly from the bytes... I'm thinking of either letting it as an array of bytes so we can come and decode it later or leave it as string because strings in go are a sequence of bytes... The danger is that we do not fix the string after or that we should fix it before.
    
    Ref.
    https://blog.golang.org/strings
    https://stackoverflow.com/questions/13510458/golang-convert-iso8859-1-to-utf8

--- pdu_test.go
+++ pdu_test.go
@@ -15,12 +15,14 @@ var enquiryLinkRespFixture, _ = hex.DecodeString("000000108000001500000000000000
 var bindTransmitterFixture, _ = hex.DecodeString("0000001f000000020000000000000000746573740074657374000034000000")
 var bindTransmitterRespFixture, _ = hex.DecodeString("000000158000000200000000000000007465737400")
 var submitSmRespFixture, _ = hex.DecodeString("000000128000000400000000000000033100")
+var deliverSmOptionsFixture, _ = hex.DecodeString("0000003f000000050000000000000001000000000000353134393432363635300004000000000000000000001e000631313130370004270001020425000100")
 
 var enquiryLinkObjHeader = Header{commandLength: 16, commandId: "enquire_link", commandStatus: "ESME_ROK", sequenceNumber: 0}
 var enquiryLinkRespObjHeader = Header{commandLength: 16, commandId: "enquire_link_resp", commandStatus: "ESME_ROK", sequenceNumber: 0}
 var bindTransmitterObjHeader = Header{commandLength: 31, commandId: "bind_transmitter", commandStatus: "ESME_ROK", sequenceNumber: 0}
 var bindTransmitterRespObjHeader = Header{commandLength: 21, commandId: "bind_transmitter_resp", commandStatus: "ESME_ROK", sequenceNumber: 0}
 var submitSmRespObjHeader = Header{commandLength: 18, commandId: "submit_sm_resp", commandStatus: "ESME_ROK", sequenceNumber: 0}
+var deliverSmRespObjHeader = Header{commandLength: 63, commandId: "deliver_sm", commandStatus: "ESME_ROK", sequenceNumber: 1}
 
 var bindTransmitterObjBody = Body{
 	mandatoryParameter: map[string]interface{}{
@@ -35,6 +37,33 @@ var bindTransmitterObjBody = Body{
 }
 var bindTransmitterRespObjBody = Body{mandatoryParameter: map[string]interface{}{"system_id": "test"}}
 var submitSmRespObjBody = Body{mandatoryParameter: map[string]interface{}{"message_id": "1"}}
+var deliverSmObjBody = Body{
+	mandatoryParameter: map[string]interface{}{
+		"service_type":            "",
+		"source_addr_ton":         0,
+		"source_addr_npi":         0,
+		"source_addr":             "",
+		"dest_addr_ton":           0,
+		"dest_addr_npi":           0,
+		"destination_addr":        "5149426650",
+		"esm_class":               4,
+		"protocol_id":             0,
+		"priority_flag":           0,
+		"schedule_delivery_time":  "",
+		"validity_period":         "",
+		"registered_delivery":     0,
+		"replace_if_present_flag": 0,
+		"data_coding":             0,
+		"sm_default_msg_id":       0,
+		"sm_length":               0,
+		"short_message":           "",
+	},
+	optionalParameters: []map[string]interface{}{
+		{"tag": "receipted_message_id", "length": 6, "value": "11107"},
+		{"tag": "message_state", "length": 1, "value": 2},
+		{"tag": "delivery_failure_reason", "length": 1, "value": 0},
+	},
+}
 
 var submitSmRespObj = PDU{header: submitSmRespObjHeader, body: submitSmRespObjBody}
 var bindTransmitterObj = PDU{header: bindTransmitterObjHeader, body: bindTransmitterObjBody}
@@ -77,6 +106,7 @@ func Test_parsePduBody(t *testing.T) {
 		{"parse_bind_transmitter", args{header: bindTransmitterObjHeader, bytes: bindTransmitterFixture}, bindTransmitterObjBody},
 		{"parse_bind_transmitter_resp", args{header: bindTransmitterRespObjHeader, bytes: bindTransmitterRespFixture}, bindTransmitterRespObjBody},
 		{"parse_submit_sm_resp", args{bytes: submitSmRespFixture, header: submitSmRespObjHeader}, submitSmRespObjBody},
+		{"parse_deliver_sm_with_options", args{bytes: deliverSmOptionsFixture, header: deliverSmRespObjHeader}, deliverSmObjBody},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
