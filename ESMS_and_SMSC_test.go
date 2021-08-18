4e954853dcbbeed5f04f1ae6b9da74bbf99899f6
elafontaine <eric.lafontaine1@gmail.com>
2021-08-18 16:42:04 -0400

    Add networking of client and server (TCP)
    The commit evolve the network test to handle errors and send a SMPP object into the write.  We handle the reading as well as the bytes (not a SMPP object).
    
    This test will have to evolve to be more user-friendly and require to pursue the pdu_builder for submit_sm.

--- ESMS_and_SMSC_test.go
+++ ESMS_and_SMSC_test.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"net"
 	"testing"
+	"time"
 )
 
 const (
@@ -27,8 +28,41 @@ func TestServerInstantiationAndConnectClient(t *testing.T) {
 		t.Errorf("couldn't connect client to server successfully: %v", err)
 	}
 	defer Esme.Close()
-	expectedBytes := []byte("hello")
-	Esme.clientSocket.Write(expectedBytes)
+	pdu := PDU{
+		header: Header{
+			sequenceNumber: 1,
+			commandId:      "submit_sm",
+			commandStatus:  "ESME_OK",
+			commandLength:  0,
+		},
+		body: Body{
+			mandatoryParameter: map[string]interface{}{
+				"service_type":            "",
+				"source_addr_ton":         0,
+				"source_addr_npi":         0,
+				"source_addr":             "5555551234",
+				"dest_addr_ton":           0,
+				"dest_addr_npi":           0,
+				"destination_addr":        "5555551234",
+				"esm_class":               0,
+				"protocol_id":             0,
+				"priority_flag":           0,
+				"schedule_delivery_time":  "",
+				"validity_period":         "",
+				"registered_delivery":     0,
+				"replace_if_present_flag": 0,
+				"data_coding":             0,
+				"sm_default_msg_id":       0,
+				"sm_length":               0,
+				"short_message":           "",
+			},
+		},
+	}
+	expectedBytes, err := EncodePdu(pdu)
+	if err != nil {
+		t.Errorf("Couldn't get the bytes out of the PDU: %s", err)
+	}
+	_, err = Esme.clientSocket.Write(expectedBytes)
 	readBuf, err := AcceptNewConnectionAndReadFromSMSC(smsc)
 	if err != nil {
 		t.Errorf("Couldn't read on a newly established Connection: %v", err)
@@ -45,7 +79,14 @@ func AcceptNewConnectionAndReadFromSMSC(smsc SMSC) (readBuf []byte, err error) {
 		return nil, err
 	}
 	readBuf = make([]byte, 4096)
+	err = serverConnectionSocket.SetDeadline(time.Now().Add(1 * time.Second))
+	if err != nil {
+		return nil, err
+	}
 	_, err = serverConnectionSocket.Read(readBuf)
+	if err != nil {
+		return nil, err
+	}
 	return readBuf, err
 }
 
