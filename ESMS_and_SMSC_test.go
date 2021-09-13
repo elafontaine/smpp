4f21480b75f96c72c9b0b5705d7e9fb06c826342
elafontaine <eric.lafontaine1@gmail.com>
2021-09-12 22:38:30 -0400

    Use pdu builder in ESME tests

--- ESMS_and_SMSC_test.go
+++ ESMS_and_SMSC_test.go
@@ -30,37 +30,8 @@ func TestServerInstantiationAndConnectClient(t *testing.T) {
 		t.Errorf("couldn't connect client to server successfully: %v", err)
 	}
 	defer Esme.Close()
-	pdu := PDU{
-		header: Header{
-			sequenceNumber: 1,
-			commandId:      "submit_sm",
-			commandStatus:  "ESME_OK",
-			commandLength:  0,
-		},
-		body: Body{
-			mandatoryParameter: map[string]interface{}{
-				"service_type":            "",
-				"source_addr_ton":         0,
-				"source_addr_npi":         0,
-				"source_addr":             "5555551234",
-				"dest_addr_ton":           0,
-				"dest_addr_npi":           0,
-				"destination_addr":        "5555551234",
-				"esm_class":               0,
-				"protocol_id":             0,
-				"priority_flag":           0,
-				"schedule_delivery_time":  "",
-				"validity_period":         "",
-				"registered_delivery":     0,
-				"replace_if_present_flag": 0,
-				"data_coding":             0,
-				"sm_default_msg_id":       0,
-				"sm_length":               0,
-				"short_message":           "",
-			},
-		},
-	}
-	expectedBytes, err := EncodePdu(pdu)
+	pdu := NewSubmitSM()
+	expectedBytes, err := EncodePdu(*pdu)
 	if err != nil {
 		t.Errorf("Couldn't get the bytes out of the PDU: %s", err)
 	}
