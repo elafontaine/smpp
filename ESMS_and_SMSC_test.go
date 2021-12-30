48ffcc3d1457a18a8456eaeacdf8d8ad12d4f244
elafontaine <eric.lafontaine1@gmail.com>
2021-12-29 23:23:24 -0500

    test 2 connections on the same smsc
    This is far from the ideal solution as we're still in the same "thread'
    of logical execution.  However, it added 2 new things for the tests;
    - an exposed stucture for the connections
    - a way to read from specific connections
    
    This will probably evolve to have the SMSC have it's own co-routines
    started to accept new incoming connections automatically.  It should
    probably also get to a point where we should be able to get the errors
    encountered in the SMSC co-routine... Problem for futur me/contributor.

--- ESMS_and_SMSC_test.go
+++ ESMS_and_SMSC_test.go
@@ -28,13 +28,14 @@ func TestServerInstantiationAndConnectClient(t *testing.T) {
 		t.Errorf("couldn't connect client to server successfully: %v", err)
 	}
 	defer Esme.Close()
-	err2 := Esme.bindTransmiter("SystemId", "Password")
+	err2 := Esme.bindTransmiter("SystemId", "Password")  //Should we expect the bind_transmitter to return only when the bind is done and valid? 
 	if err2 != nil {
 		t.Errorf("Couldn't write to the socket PDU: %s", err)
 	}
-	readBuf, err := AcceptNewConnectionAndReadFromSMSC(smsc)
-	if err != nil {
-		t.Errorf("Couldn't read on a newly established Connection: %v", err)
+	_, err = AcceptNewConnectionAndReadFromSMSC(&smsc)
+	readBuf, err2 := readFromConnection(smsc.connections[0])
+	if err != nil || err2 != nil{
+		t.Errorf("Couldn't read on a newly established Connection: \n err =%v\n err2 =%v", err, err2)
 	}
 	expectedBuf, err := EncodePdu(NewBindTransmitter().WithSystemId(validSystemID).WithPassword(validPassword))
 	tempReadBuf := readBuf[0:len(expectedBuf)]
@@ -43,23 +44,72 @@ func TestServerInstantiationAndConnectClient(t *testing.T) {
 	}
 }
 
+func TestCanWeConnectTwiceToSMSC(t *testing.T) {
+	smsc, err := StartSmscSimulatorServer()
+	if err != nil {
+		t.Errorf("couldn't start server successfully: %v", err)
+	}
+	defer smsc.Close()
+
+	Esme, err := InstantiateEsme(smsc.listeningSocket.Addr())
+	if err != nil {
+		t.Errorf("couldn't connect client to server successfully: %v", err)
+	}
+	defer Esme.Close()
+
+	Esme2, err := InstantiateEsme(smsc.listeningSocket.Addr()) 
+	if err != nil {
+		t.Errorf("couldn't connect client to server successfully: %v", err)
+	}
+	defer Esme2.Close()
+	err2 := Esme2.bindTransmiter("SystemId", "Password")  //Should we expect the bind_transmitter to return only when the bind is done and valid? 
+	if err2 != nil {
+		t.Errorf("Couldn't write to the socket PDU: %s", err)
+	}
+	_, err = AcceptNewConnectionAndReadFromSMSC(&smsc)
+	_, err2 = AcceptNewConnectionAndReadFromSMSC(&smsc)
+
+	readBuf2, err3 := readFromConnection(smsc.connections[1])
+
+	if err != nil || err2 != nil || err3 != nil {
+		t.Errorf("Couldn't read on a newly established Connection: \n err =%v\n err2 =%v\n err3 =%v", err, err2, err3)
+	}
+	expectedBuf, err := EncodePdu(NewBindTransmitter().WithSystemId(validSystemID).WithPassword(validPassword))
+	tempReadBuf := readBuf2[0:len(expectedBuf)]
+	if !bytes.Equal(tempReadBuf, expectedBuf) || err != nil {
+		t.Errorf("We didn't receive what we sent")
+	}
+	if !assertWeHaveActiveConnections(&smsc,2) {
+		t.Errorf("We didn't have the expected amount of connections!")
+	}
+}
+
+func assertWeHaveActiveConnections(smsc *SMSC, number_of_connections int) (is_right_number bool){
+	if len(smsc.connections) == number_of_connections {
+		return true
+	} else {
+		return false
+	}
+}
 
 // possibly some production functions... need to confirm with another tests maybe ?
-func AcceptNewConnectionAndReadFromSMSC(smsc SMSC) (readBuf []byte, err error) {
+func AcceptNewConnectionAndReadFromSMSC(smsc *SMSC) (readBuf []byte, err error) {
 	serverConnectionSocket, err := smsc.listeningSocket.Accept()
 	if err != nil {
 		err = fmt.Errorf("Couldn't establish connection on the server side successfully: %v", err)
 		return nil, err
 	}
-	readBuf = make([]byte, 4096)
-	err = serverConnectionSocket.SetDeadline(time.Now().Add(1 * time.Second))
+	smsc.connections = append(smsc.connections, serverConnectionSocket)
+	return readBuf, err
+}
+
+func readFromConnection(serverConnectionSocket net.Conn) ([]byte, error) {
+	readBuf := make([]byte, 4096)
+	err := serverConnectionSocket.SetDeadline(time.Now().Add(1 * time.Second))
 	if err != nil {
 		return nil, err
 	}
 	_, err = serverConnectionSocket.Read(readBuf)
-	if err != nil {
-		return nil, err
-	}
 	return readBuf, err
 }
 
@@ -71,6 +121,7 @@ func InstantiateEsme(serverAddress net.Addr) (esme ESME, err error) {
 
 func StartSmscSimulatorServer() (smsc SMSC, err error) {
 	serverSocket, err := net.Listen(connType, connhost+":"+connport)
-	smsc = SMSC{serverSocket}
+	conns := []net.Conn{}
+	smsc = SMSC{serverSocket, conns}
 	return smsc, err
 }
--- SMSC.go
+++ SMSC.go
@@ -4,6 +4,12 @@ import "net"
 
 type SMSC struct {
 	listeningSocket net.Listener
+	connections []net.Conn
+}
+
+func NewSMSC(listeningSocket net.Listener) (s *SMSC) {
+	s = &SMSC{listeningSocket: listeningSocket, connections: []net.Conn{}}
+	return s
 }
 
 func (s SMSC) Close() {
